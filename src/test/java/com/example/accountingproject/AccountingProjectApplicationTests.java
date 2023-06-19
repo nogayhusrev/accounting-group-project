46bbf31ff6f7c558b1de457135d736d35940658e
nogayhusrev <nogayhusrev@gmail.com>
2023-06-19 22:28:29 +0300

    Initial Commit

---
+++ src/test/java/com/example/accountingproject/AccountingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
