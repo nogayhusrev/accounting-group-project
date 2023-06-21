54917582cf2b6f1bbd18f1b4abb978f5f25c17f8
nogayhusrev <nogayhusrev@gmail.com>
2023-06-21 11:54:18 +0300

    Adam, August

--- src/main/java/com/example/accountingproject/AccountingProjectApplication.java
+++ src/main/java/com/example/accountingproject/AccountingProjectApplication.java
@@ -1,13 +1,20 @@
 package com.example.accountingproject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountingProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountingProjectApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
